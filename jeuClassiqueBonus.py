441af0b3e15da8424d595a9d1781fc21c162c2ff
Loïc Capdeville <caploic@outlook.fr>
2025-05-27 09:48:21 +0200

    qsdqsd

--- jeuClassiqueBonus.py
+++ jeuClassiqueBonus.py
@@ -178,6 +178,7 @@ def run_game_classique_bonus(weights=None, render=False, manual=False):
             if pipe.x + PIPE_WIDTH < 0:
                 pipes.remove(pipe)
                 pipes.append(Pipe(300 * NUM_PIPES - PIPE_WIDTH))
+                score += 1
                 pipe_count += 1
                 if pipe_count % 5 == 0:
                     if PIPE_GAP > 60:
